dc9e49f6282c4b0792d4f06627bc69857c8cec86
Raizusekku <neupanenischal1@gmail.com>
2020-01-14 15:36:37 -0600

    added a Constants.java file skeleton to be consistent

---
+++ src/main/java/utils/Constants.java
@@ -0,0 +1,15 @@
+package utils;
+
+public class Constants {
+    public static final String BOT_OWNER_ID = "";
+    public static final String D_PREFIX = ".";
+    public static final String OWNER_NAME = "";
+    public static final String BOT_ID = "";
+    public static final String CLIENT_SECRET_CODE = "Your Secret Code Here";
+    public static final String SERVER_INVITE = "";
+    public static final String SUCCESS = "\u2611";
+    public static final String WARNING = "\u2622";
+    public static final String ERROR = "\u274C";
+    public static final String BOT_INVITE = "";
+    public static final String BOT_ERROR_CHANNEL = "";
+}
