34cd0c28433250b4ec83bd9b498a41ac3757246f
Raizusekku <neupanenischal1@gmail.com>
2020-08-01 08:20:10 -0500

    Updated Constants and ImageURLUtil

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -2,14 +2,15 @@ package utils;
 
 public class Constants {
     public static final String BOT_OWNER_ID = "";
-    public static final String D_PREFIX = ".";
-    public static final String OWNER_NAME = "";
+    public static final String D_PREFIX = "";
     public static final String BOT_ID = "";
-    public static final String CLIENT_SECRET_CODE = "Your Secret Code Here";
+    public static final String BOT_RELEASE_CODE = "";
+    public static final String BOT_TEST_CODE = "";
     public static final String SERVER_INVITE = "";
     public static final String SUCCESS = "\u2611";
     public static final String WARNING = "\u2622";
     public static final String ERROR = "\u274C";
     public static final String BOT_INVITE = "";
     public static final String BOT_ERROR_CHANNEL = "";
+    public static final String IMGURID = "";
 }
