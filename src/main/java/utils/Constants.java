86facabd5ec8678326f93281c59b8fa3bcbe9ec9
Raizusekku <neupanenischal1@gmail.com>
2020-08-03 22:47:53 -0500

    updated pom

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -1,16 +1,16 @@
 package utils;
 
 public class Constants {
-    public static final String BOT_OWNER_ID = "222488511385698304";
-    public static final String D_PREFIX = ".";
-    public static final String BOT_ID = "390731856456450066";
-    public static final String BOT_RELEASE_CODE = "MzkwNzMxODU2NDU2NDUwMDY2.WjIHZg.0cbrbqY2ZlUcG8dfBLNc432zAvY";
-    public static final String BOT_TEST_CODE = "NDE0MjA4NDExNjM5NDgwMzIz.WodvpA.3Tv9GnKyrrM1BoqdMS-GXasicno";
-    public static final String SERVER_INVITE = "https://discord.com/invite/rX2amsb";
+    public static final String BOT_OWNER_ID = "";
+    public static final String D_PREFIX = "";
+    public static final String BOT_ID = "";
+    public static final String BOT_RELEASE_CODE = "";
+    public static final String BOT_TEST_CODE = "";
+    public static final String SERVER_INVITE = "";
     public static final String SUCCESS = "\u2611";
     public static final String WARNING = "\u2622";
     public static final String ERROR = "\u274C";
-    public static final String BOT_INVITE = "https://discordapp.com/oauth2/authorize?client_id=390731856456450066&scope=bot&permissions=2146827519";
-    public static final String BOT_ERROR_CHANNEL = "584488228749574160";
-    public static final String IMGURID = "50aa691a8a236fc";
+    public static final String BOT_INVITE = "";
+    public static final String BOT_ERROR_CHANNEL = "";
+    public static final String IMGURID = "";
 }
